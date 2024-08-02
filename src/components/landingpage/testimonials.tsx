f1b4a08d483fdda9db2bfaa8d3791cee596d400e
maxwell95 <110671828+harzyz@users.noreply.github.com>
2024-08-02 00:13:42 -0700

    Update testimonials.tsx

--- src/components/landingpage/testimonials.tsx
+++ src/components/landingpage/testimonials.tsx
@@ -33,10 +33,10 @@ const Testimonials = () => {
       <h1 className="text-center text-3xl text-primary font-bold">
         Testimonials
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-5">
         {testimonials.map((item) => (
           <div key={item.id} className="bg-primary text-white p-5 rounded-[8px] flex flex-col items-center">
-            <div className="w-[80px] h-[80px] border-[5px] border-white rounded-[50%]">
+            <div className="w-[60px] md:w-[80px] h-[60px] md:h-[80px] border-[5px] border-white rounded-[50%]">
               <Image
                 className="w-full h-full rounded-[50%]"
                 src={item.imgUrl}
@@ -44,7 +44,7 @@ const Testimonials = () => {
               />
             </div>
             <h3 className="font-bold text-xl">{item.text}</h3>
-            <p className="text-center text-lg font-[300]">{item.testimonial}</p>
+            <p className="text-center text-xs md:text-sm xl:text-lg font-[300]">{item.testimonial}</p>
             <div className="flex">
               {star.map((star) => (
                 <FaStar key={star} color="#FFA500" />
